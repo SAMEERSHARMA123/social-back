e0db1ab16487468efbf5804fa8334e6454b3e9da
Sameer Sharma <jangidsameer26@gmail.com>
2025-07-08 10:37:01 +0530

    first commit

---
+++ Utils/token.js
@@ -0,0 +1,19 @@
+   const jwt = require('jsonwebtoken');
+
+   const user_token = (user) => {
+      const token = jwt.sign(
+              { id: user._id, email: user.email, name : user.name,username : user.username,bio : user.bio, profileImage :user.profileImage, followers : user.followers, following : user.following, posts : user.posts },
+              process.env.JWT_SECRET,
+              { expiresIn: '7d' }
+            );
+
+            return token
+
+            
+   }
+
+
+   module.exports = {user_token}
+
+
+   
